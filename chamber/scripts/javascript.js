1c344bbe231ce48ab12e3353ec6410c311c01878
edemfrancis <efrancis.edem@gmail.com>
2024-05-09 13:52:52 +0100

    added cahmber to site

---
+++ chamber/scripts/javascript.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("#button");
+const navList = document.querySelector(".horizontal");
+
+button.addEventListener("click", () => {
+    navList.classList.toggle("open");
+    button.classList.toggle("open");
+})
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.querySelector(".span3").textContent = new Date().toLocaleDateString('en-US', options);
+
+const userName = "Edem Francis";
+document.querySelector(".span2").textContent = userName;
